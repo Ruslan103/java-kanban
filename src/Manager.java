ea745e5f4a5cf6da6c1be454a30c96ca816cc774
Ruslan103 <117119513+Ruslan103@users.noreply.github.com>
2022-12-18 02:56:54 +0400

    сделал п.2.1-3.1 ТЗ

--- src/Manager.java
+++ src/Manager.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Manager {
     double taskId = 0.1;
     double subtaskId = 0.3;
     double epicId = 0.2;
     HashMap<Double, Epic> epics = new HashMap<>(); // мапа с эпиком и его подзадачами
-    ///ArrayList<Epic> epics = new ArrayList<>();
-
     ArrayList<Subtask> subtasksList = new ArrayList<>(); // список с подзадачами
     HashMap<Double, Subtask> subtasks = new HashMap<>();
     HashMap<Double, Task> tasks = new HashMap<>(); // список с задачами
@@ -20,11 +19,12 @@ public class Manager {
         epicId = epic.getId();
         epics.put(epicId, epic);
 
-        for (int i = 0; i < subtasksList.size(); i++) {
-            epic.subtasksId.add(subtasksList.get(i).getId());
+        for (Subtask subtask : subtasksList) {
+            epic.subtasksId.add(subtask.getId());
         }
         subtasksList = new ArrayList<>();
     }
+
 
     void CreateSubtasks(String title, String description, String status) {
 
@@ -66,35 +66,42 @@ public class Manager {
     }
 
     String getTitleById(double id) { // Получение по идентификатору (п.2.3 ТЗ)
+
         String title = "Задача по id не найдена";
-        for (Double i : epics.keySet()) {
-            if (id == i) {
-                title = epics.get(i).getTitle();
-                return title;
+        if (id % 10 == 0.1) {
+            for (Double i : epics.keySet()) {
+                if (id == i) {
+                    title = epics.get(i).getTitle();
+                    return title;
+                }
             }
         }
-        for (Double i : tasks.keySet()) {
-            if (id == i) {
-                title = tasks.get(i).getTitle();
-                return title;
+        if (id % 10 == 0.2) {
+            for (Double i : tasks.keySet()) {
+                if (id == i) {
+                    title = tasks.get(i).getTitle();
+                    return title;
+                }
             }
         }
-        for (Double i : subtasks.keySet()) {
-            if (id == i) {
-                title = subtasks.get(i).getTitle();
-                return title;
+        if (id % 10 == 0.3) {
+            for (Double i : subtasks.keySet()) {
+                if (id == i) {
+                    title = subtasks.get(i).getTitle();
+                    return title;
+                }
             }
         }
         return title;
     }
 
-    void CreateTask(Task task) { // создание задачи
+    void CreateTask(Task task) { // создание задачи п.2.4
         task.setId(taskId + 1);
         taskId = task.getId();
         tasks.put(taskId, task);
     }
 
-    void CreateEpic(Epic epic) {  // создание эпика
+    void CreateEpic(Epic epic) {  // создание эпика п.2.4
         epic.setId(epicId + 1);
         epicId = epic.getId();
         epics.put(epicId, epic);
@@ -104,14 +111,70 @@ public class Manager {
         subtasksList = new ArrayList<>();
         epics.put(epicId, epic);
     }
-    void CreateEpic (Subtask subtask){ //создание подзадачи
+
+    void CreateEpic(Subtask subtask) { //создание подзадачи п.2.4
         subtask.setId(subtaskId + 1);
         subtaskId = subtask.getId();
         subtasksList.add(subtask);
         subtasks.put(subtaskId, subtask);
     }
 
+
+
+    void updateTask(Double id, Task task) { //  обновление задачи п.2.5
+        tasks.put(id, task);
+    }
+
+    void updateEpics(Double id, Epic epic) { // обновление эпика п.2.5
+        epics.put(id, epic);
+    }
+
+    void updateSubtasks(Double id, Subtask subtask) {// обновление подзадачи п.2.5
+        subtasks.put(id, subtask);
+    }
+
+
+
+    void removeForId(Double id) { // удаление по id п.2.6
+        if (id % 1 == 0.1) { // если остаток от деления на 1 равен 0,1 то проходим по мапе с задачами
+            tasks.remove(id);
+        } else if (id % 1 == 0.2) { // если остаток от деления на 1 равен 0.2 то проходим по эпикам
+            epics.remove(id);
+        } else if (id % 1 == 0.3) { // если остаток от деления на 1 равен 0,3 то проходим по подзадачам
+            subtasks.remove(id); // удаляю подзадачу из мапы подзадач
+            for (Double i : epics.keySet()) { // прохожжу циклом по значениям мапы эпика, чтобы удалить из него id
+                for (int j = 0; j < epics.get(i).subtasksId.size(); j++) { // прохожу по списку id подзадач в каждом эпике
+                    if (epics.get(i).subtasksId.get(j).equals(id)) {
+                        epics.get(i).subtasksId.remove(id); // удаляем id из хэш мапы
+                    }
+                }
+            }
+        }
+
+    }
+
+    ArrayList<String> getSubtask(Epic epic) { // получение подзадач эпика п.3.1
+        ArrayList<String> titleSubtask = new ArrayList<>();
+        for (Epic i : epics.values()) {// проходим циклом по значениям мапы эпика
+            if (i.equals(epic)) {// если принятый эпик равен эпикку в мапе то создаем список id одзадач эпика:
+                ArrayList<Double> id = epic.subtasksId;
+                for (Double j : subtasks.keySet()) { // прохожу по ключам мапы подзадач
+                    for (Double k : id) { // прохожу по списку id
+                        if (Objects.equals(j, k)) { // сравниваю ключ мапы подзадач с  каждым элементом из списка id если равны:
+                            titleSubtask.add(subtasks.get(j).getTitle());// то записываю наименование задачи в titleSubtask
+                        }
+                    }
+                }
+            }
+        }
+        return titleSubtask; // возвращаю список с названием задач
+    }
+    void status (String status){
+
+    }
 }
+
+
 
 
 
